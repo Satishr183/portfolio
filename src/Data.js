9da9f226b4fd97c528d3a87c6a243df69c3196fc
Satish <satish_95raj@outlook.com>
2023-01-10 19:31:16 +0530

    deploy

---
+++ src/Data.js
@@ -0,0 +1,55 @@
+export const projects = [
+    {
+      title: "Lorem Ipsum Generator",
+      subtitle: "HTML|CSS|JS",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://i.pinimg.com/originals/af/66/86/af66864dfb8cf1701c08983060926216.gif",
+      link: "https://github.com/Satishr183/loremGen.github.io",
+    },
+    {
+      title: "BLOGG SITE Backend",
+      subtitle: "NodeJS|mongoDB|Express|JWT",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://payu.in/blog/wp-content/uploads/2019/01/Blog-Cover.gif",
+      link: "https://github.com/Satishr183/blogging-site.P1",
+    },
+    {
+      title: "Open To Intern Backend",
+      subtitle: "NodeJS|mongoDB|AWS-S3|Express",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://static.schibsted.com/wp-content/uploads/2022/11/30152440/giphy-2.gif",
+      link: "https://github.com/Satishr183/open-to-intern-P2",
+    },
+    {
+      title: "Book-Management Backend",
+      subtitle: "NodeJS|mongoDB|AWS-S3|Express|JWT",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://cdn.dribbble.com/users/527271/screenshots/2470948/loop_2.gif",
+      link: "https://github.com/Satishr183/BookManagementP3",
+    },
+    {
+        title: "Url-Shortner",
+        subtitle: "NodeJS|mongoDB|Redis-Cache|Express|JWT",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "https://ph-files.imgix.net/9b30240c-138f-4a5f-8711-a1b16f0eef60.gif?auto=format",
+        link: "https://github.com/Satishr183/ShortUrl-Project",
+      },
+      {
+        title: "E-Commerce Backend",
+        subtitle: "NodeJS|mongoDB|AWS-S3|Express|JWT",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "https://i.pinimg.com/originals/66/22/ab/6622ab37c6db6ac166dfec760a2f2939.gif",
+        link: "https://github.com/Satishr183/E-Commerce-backend",
+      },
+      
+  ];
+
+  export const skills= [
+   "NODEJS","JavaScript","HTML","CSS","MONGODB","AWS-S3","Redis","Express","Bootstrap","ReactJS","MYSQL"
+  ]
