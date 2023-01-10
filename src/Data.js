26da5d6716a0ea9fdaa6b9349d4e9a429277b7db
Satish <satish_95raj@outlook.com>
2023-01-10 22:27:31 +0530

    deploy

--- src/Data.js
+++ src/Data.js
@@ -3,23 +3,21 @@ export const projects = [
       title: "Lorem Ipsum Generator",
       subtitle: "HTML|CSS|JS",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        "Lorem Ipsum is simply dummy text of the printing",
       image: "https://i.pinimg.com/originals/af/66/86/af66864dfb8cf1701c08983060926216.gif",
       link: "https://github.com/Satishr183/loremGen.github.io",
     },
     {
       title: "BLOGG SITE Backend",
       subtitle: "NodeJS|mongoDB|Express|JWT",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      description:"Publish your post and perform crud operation on the blog post until you are authenticate user.",
       image: "https://payu.in/blog/wp-content/uploads/2019/01/Blog-Cover.gif",
       link: "https://github.com/Satishr183/blogging-site.P1",
     },
     {
       title: "Open To Intern Backend",
       subtitle: "NodeJS|mongoDB|AWS-S3|Express",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      description:"Internship for college where youn enroll for internship in college it similar to internshala",
       image: "https://static.schibsted.com/wp-content/uploads/2022/11/30152440/giphy-2.gif",
       link: "https://github.com/Satishr183/open-to-intern-P2",
     },
@@ -27,7 +25,7 @@ export const projects = [
       title: "Book-Management Backend",
       subtitle: "NodeJS|mongoDB|AWS-S3|Express|JWT",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        "It is book-management backend application where you need to authenticate and authorize to write/publish book and perform CRUD operations.",
       image: "https://cdn.dribbble.com/users/527271/screenshots/2470948/loop_2.gif",
       link: "https://github.com/Satishr183/BookManagementP3",
     },
@@ -35,7 +33,7 @@ export const projects = [
         title: "Url-Shortner",
         subtitle: "NodeJS|mongoDB|Redis-Cache|Express|JWT",
         description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+          "It is similar bitly which convert long url to short url and for better performnce I used caching(Redis)",
         image: "https://ph-files.imgix.net/9b30240c-138f-4a5f-8711-a1b16f0eef60.gif?auto=format",
         link: "https://github.com/Satishr183/ShortUrl-Project",
       },
@@ -43,7 +41,7 @@ export const projects = [
         title: "E-Commerce Backend",
         subtitle: "NodeJS|mongoDB|AWS-S3|Express|JWT",
         description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+          "E-commerce application like flipkar, amazon where you add your product to cart and placed your order.",
         image: "https://i.pinimg.com/originals/66/22/ab/6622ab37c6db6ac166dfec760a2f2939.gif",
         link: "https://github.com/Satishr183/E-Commerce-backend",
       },
